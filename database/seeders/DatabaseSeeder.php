ef88c5664a8d34e9a6ff133426fc75be37167ef1
fabrisdev <ezepuntocom@hotmail.com>
2023-09-26 10:38:54 -0300

    Seeder actualizado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,7 +7,6 @@ use App\Models\Lote;
 use App\Models\LoteFormadoPor;
 use App\Models\Paquete;
 use App\Models\Alojamiento;
-use App\Models\SedeHogar;
 use App\Models\Sede;
 
 class DatabaseSeeder extends Seeder
@@ -23,16 +22,14 @@ class DatabaseSeeder extends Seeder
             "id" => 1,
             "direccion" => "Avenida brasil 1295"
         ]);
-        SedeHogar::create([
-            "id" => 1
-        ]);
         Sede::create([
             "id" => 1
         ]);
         Paquete::create([
             "id" => 1,
             "peso_en_kg" => 5,
-            "destino" => 1
+            "destino" => 1,
+            "email" => "persona@gmail.com"
         ]);
         Lote::create([
             "id" => 1,
