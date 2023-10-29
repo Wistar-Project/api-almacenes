650be93cedfea97e0f80b0e42acfd94f1770c056
fabrisdev <ezepuntocom@hotmail.com>
2023-10-29 19:22:51 -0300

    Relaciones necesarias agregadas

--- app/Models/Lote.php
+++ app/Models/Lote.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\HasOne;
 class Lote extends Model
 {
     use HasFactory;
@@ -12,4 +12,9 @@ class Lote extends Model
     protected $fillable = [
         'destino'
     ];
+
+    public function camion(): HasOne
+    {
+        return $this->hasOne(LoteAsignadoACamion::class, "id_paquete");
+    }
 }
--- app/Models/Paquete.php
+++ app/Models/Paquete.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Paquete extends Model
 {
@@ -14,4 +15,14 @@ class Paquete extends Model
         'destino',
         'email'
     ];
+
+    public function pickup(): HasOne
+    {
+        return $this->hasOne(PaqueteAsignadoAPickup::class, "id_paquete");
+    }
+
+    public function lote(): HasOne
+    {
+        return $this->hasOne(LoteFormadoPor::class, "id_paquete");
+    }
 }
