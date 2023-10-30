13e8f45ceb55dbaf65623158d871fa45d137e315
fabrisdev <ezepuntocom@hotmail.com>
2023-10-29 21:40:23 -0300

    Validación de que tengan mismo destino agregada

--- app/Http/Controllers/LoteController.php
+++ app/Http/Controllers/LoteController.php
@@ -9,6 +9,7 @@ use App\Models\Lote;
 use Illuminate\Http\Request;
 use App\Models\Alojamiento;
 use App\Models\LoteAsignadoACamion;
+use Illuminate\Support\Facades\Validator;
 
 class LoteController extends Controller
 {
@@ -51,17 +52,15 @@ class LoteController extends Controller
     }
 
     public function AsignarPaquete(Request $request){
-        $lote = $request -> post("lote");
-        $paquete = $request -> post("paquete");
-        $loteEncontrado = Lote::find($lote); 
-        $paqueteEncontrado = Paquete::find($paquete);
-        if($loteEncontrado == null || $paqueteEncontrado == null){
+        $lote = Lote::findOrFail($request -> post("lote")); 
+        $paquete = Paquete::findOrFail($request -> post("paquete"));
+        if($lote -> destino != $paquete -> destino){
             $BAD_REQUEST_HTTP = 400;
-            abort($BAD_REQUEST_HTTP, "El lote o paquete ingresado no existe");
+            abort($BAD_REQUEST_HTTP, "Ambos deben tener el mismo destino");
         }
-        LoteFormadoPor::create([
-            "id_lote" => $lote,
-            "id_paquete" => $paquete
+        return LoteFormadoPor::create([
+            "id_lote" => $request -> post('lote'),
+            "id_paquete" => $request -> post('paquete')
         ]);
     }
 
