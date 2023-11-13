81ec1b5048fc481ec1a3d329392f9c59975e63de
fabrisdev <ezepuntocom@hotmail.com>
2023-11-13 19:23:33 -0300

    Nuevos tests sobre paquetes

--- tests/Feature/PaqueteTest.php
+++ tests/Feature/PaqueteTest.php
@@ -3,9 +3,11 @@
 namespace Tests\Feature;
 
 use App\Models\Administrador;
+use App\Models\Alojamiento;
 use App\Models\Conductor;
 use App\Models\Funcionario;
 use App\Models\Persona;
+use App\Models\Sede;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -96,6 +98,83 @@ class PaqueteTest extends TestCase
         $response->assertStatus(401);
         $response->assertExactJson([
             "message" => "Unauthenticated."
+        ]);
+    }
+
+    public function test_ver_informacion_de_paquete(){
+        Paquete::create([
+            "id" => 10,
+            "peso_en_kg" => 5,
+            "destino" => 1,
+            "email" => "prueba@gmail.com"
+        ]);
+        $response = $this -> actingAs($this->crearAdministrador())->get('/api/v1/paquetes/10');
+        $response -> assertStatus(200);
+        $response -> assertJson([
+            "id" => 10,
+            "pesoEnKg" => 5,
+            "direccionDestino" => "Dirección 1",
+            "vehiculoAsignado" => "Ninguno",
+            "email" => "prueba@gmail.com",
+            "loteAsignado" => "Ninguno"
+        ]);
+    }
+
+    public function test_ver_informacion_de_paquete_sin_autenticarse(){
+        $response = $this ->get('/api/v1/paquetes/10', [
+            "Accept" => "application/json"
+        ]);
+        $response -> assertStatus(401);
+        $response -> assertExactJson([
+            "message" => "Unauthenticated."
+        ]);
+    }
+
+    public function test_mostrar_paquetes_para_asignar(){
+        Alojamiento::create([ "id" => 11, "direccion" => "Dirección 11"]);
+        Sede::create([ "id" => 11 ]);
+        Paquete::create([
+            "id" => 11,
+            "peso_en_kg" => 5,
+            "destino" => 11,
+            "email" => "a@gmail.com"
+        ]);
+        Paquete::create([
+            "id" => 12,
+            "peso_en_kg" => 5,
+            "destino" => 11,
+            "email" => "a@gmail.com"
+        ]);
+        Paquete::create([
+            "id" => 13,
+            "peso_en_kg" => 5,
+            "destino" => 11,
+            "email" => "a@gmail.com"
+        ]);
+        $response = $this -> actingAs($this->crearAdministrador())->get('/api/v1/paquetes/asignar/11');
+        $response -> assertStatus(200);
+        $response -> assertExactJson([
+            11, 12, 13
+        ]);
+    }
+
+    public function test_mostrar_paquetes_para_asignar_no_autenticado(){
+        $response = $this ->get('/api/v1/paquetes/asignar/11', [
+            "Accept" => "application/json"
+        ]);
+        $response -> assertStatus(401);
+        $response -> assertExactJson([
+            "message" => "Unauthenticated."
+        ]);
+    }
+
+    public function test_mostrar_paquetes_para_asignar_siendo_conductor(){
+        $response = $this -> actingAs($this->crearChofer())->get('/api/v1/paquetes/asignar/11', [
+            "Accept" => "application/json"
+        ]);
+        $response -> assertStatus(401);
+        $response -> assertExactJson([
+            "message" => "No tienes permiso para ver esto."
         ]);
     }
 }
